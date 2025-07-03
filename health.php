73dcbc0798c081c9c8fe245dfc226c4a729cbac3
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-07-03 17:12:44 -0300

    versão: 20250703

--- health.php
+++ health.php
@@ -1,6 +1,6 @@
 <?php
 define("MOODLE_INTERNAL", true);
-define("MATURITY_ALPHA", true);
+define("MATURITY_STABLE", true);
 $plugin = new stdClass();
 include_once("version.php");
 echo "release: $plugin->release<br>";
--- version.php
+++ version.php
@@ -5,8 +5,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_suap';
-$plugin->release   = '0.1.012';
-$plugin->version = 20250624012;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->release   = '0.1.013';
+$plugin->version = 20250703013;
+$plugin->maturity = MATURITY_STABLE;
 
 $plugin->requires = 2016070700;
