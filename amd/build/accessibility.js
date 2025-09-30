d7e4e1423b646ced7125ef3a3985d5622d822a76
Matheus Mathias <mathias.matheus76@gmail.com>
2025-09-30 16:14:17 -0300

    feat: via js recuperar preferências do usuário #67

--- amd/build/accessibility.js
+++ amd/build/accessibility.js
@@ -1,17 +1,22 @@
-define(["core/str"], function(str) {
+define(["core/str", "core_user/repository"], function(str, Repository) {
 
     function activeAccessibility() {
         let checkboxes = document.querySelectorAll('.custom-checkbox-access input[type="checkbox"]');
 
         checkboxes.forEach(checkbox => {
-            checkbox.addEventListener('click', (event) => {
+            checkbox.addEventListener('change', (event) => {
                 let target = event.target;
-                if (checkbox.checked) {
-                    document.body.classList.add(`accessibility-${target.id}`);
+                const checked = target.checked;
+                const className = `accessibility-${target.id}`;
+                const prefName = `theme_suap_accessibility-${target.id}`;
+
+                if (checked) {
+                    document.body.classList.add(className);
                 } else {
-                    document.body.classList.remove(`accessibility-${target.id}`);
+                    document.body.classList.remove(className);
                 }
 
+                Repository.setUserPreference(prefName, checked);
             });
         })
 
@@ -19,6 +24,21 @@ define(["core/str"], function(str) {
 
     return {
         init: () => {
+            Repository.getUserPreferences().then(prefs => {
+                for (const key in prefs) {
+                    console.log(key + ' = ' + prefs[key]);
+                    if (key.startsWith('theme_suap_accessibility-') && prefs[key] === "1") {
+                        const id = key.replace('theme_suap_accessibility-', '');
+                        console.log(id);
+                        const input = document.getElementById(id);
+                        if (input) {
+                            input.checked = true;
+                            document.body.classList.add(`accessibility-${id}`);
+                        }
+                    }
+                }
+            });
+
             activeAccessibility();            
         }
     }
