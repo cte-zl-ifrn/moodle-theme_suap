75d9d40565ad54676544da3f42fb63e2c1e1710d
Matheus Mathias <mathias.matheus76@gmail.com>
2025-10-02 09:12:41 -0300

    fix: zoom na frontpage #67

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -188,6 +188,7 @@ $templatecontext = [
     'footer_credits_first_link_new_window' => $conf->footer_credits_first_link_new_window,
     'footer_credits_second_link_new_window' => $conf->footer_credits_second_link_new_window,
 
+    'accessibility_zoom_level' => get_user_preferences('theme_suap_accessibility_zoom_level', 100),
 ];
 // throw new Exception("Error Processing Request", 1);
 
